0cb3d575e44d31bf0f30a87f7bb57a651207eb23
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-07-24 23:29:38 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.62 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -14,3 +14,9 @@ class Solution {
         return answer;
     }
 }
+//String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+//for (int i = 0; i < numbers_arr.length; i++) {
+//    numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
+//}
+
