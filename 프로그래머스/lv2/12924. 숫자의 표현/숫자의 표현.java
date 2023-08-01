54d5f8c77e5d98c4fc0b16084cc18ed3e2b54572
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-08-01 16:51:57 +0900

    [level 2] Title: 숫자의 표현, Time: 1.50 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -18,3 +18,13 @@ class Solution {
         return answer;
     }
 }
+
+//자연수를 연속된 자연수의 합으로 표현하는 방법의 홀수 약수의 개수와 같다
+//public int expressions(int num) {
+//   int answer = 0;
+//   for (int i = 1; i <= num; i += 2) 
+//       if (num % i == 0) 
+//           answer++;
+//
+//   return answer;
+//}
