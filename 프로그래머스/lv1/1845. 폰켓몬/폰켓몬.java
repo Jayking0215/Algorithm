0ab3e330450c79ba49f95741b2f936a35d8fb06f
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-08-15 08:30:13 +0900

    [level 1] Title: 폰켓몬, Time: 2.09 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        
+        for(int tmp : nums){
+            set.add(tmp);
+        }
+        if(set.size() > nums.length / 2){
+            answer = nums.length / 2;
+        }else{
+            answer = set.size();
+        }
+        
+        return answer;
+    }
+}
