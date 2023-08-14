059a8c3bf265d8e9e36ab199d9629d5ebc6013d1
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-08-14 14:33:33 +0900

    Update 명예의 전당 （1）.java

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,7 +4,6 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         PriorityQueue<Integer> pq = new PriorityQueue<>();//낮은 수가 우선순위를 갖는다.
-        int temp = 0;
 
         for(int i = 0; i < score.length; i++) {
             pq.add(score[i]);
