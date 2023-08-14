be9a278605983f408f6b89d654b20d0624ff06cd
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-08-14 14:31:39 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.31 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,19 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();//낮은 수가 우선순위를 갖는다.
+        int temp = 0;
 
-        for (int i = 0; i < score.length; i++) {
-            list.add(score[i]);
-            Collections.sort(list, Collections.reverseOrder());
-
-            int[] arr = new int[Math.min(k, list.size())];
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = list.get(j);
+        for(int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+            if (pq.size() > k) {
+                pq.poll();
             }
-
-            Arrays.sort(arr);
-            answer[i] = arr[0];
+            answer[i] = pq.peek();
         }
 
         return answer;
