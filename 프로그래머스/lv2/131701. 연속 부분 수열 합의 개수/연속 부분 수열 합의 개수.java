d2021fd7bb909d64c8e948055ac2b2e09bd076d4
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-08-18 14:05:20 +0900

    Update 연속 부분 수열 합의 개수.java

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -7,8 +7,8 @@ class Solution {
         for(int i = 1; i <= elements.length; i++){//부분합 길이
             for(int j = 0; j < elements.length; j++){//부분합 시작위치
                 int sum = 0;
-                for(int k = 0; k < i; k++) {
-                    if(j + k > elements.length - 1){//i만큼 부분합 계산
+                for(int k = 0; k < i; k++) {//i만큼 부분합 계산
+                    if(j + k > elements.length - 1){
                         sum += elements[j + k - elements.length];
                     }else{
                         sum += elements[j + k];
