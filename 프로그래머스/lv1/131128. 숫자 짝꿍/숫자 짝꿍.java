caab4f5a08edf6415d2a2642dbcc227d0013b229
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-08-20 07:52:10 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.20 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -5,16 +5,16 @@ class Solution {
         int[] arr1 = new int[10];
         int[] arr2 = new int[10];
 
-        for(String a : X.split("")){
+        for(String a : X.split("")){//X에 포함된 숫자 갯수 count
             arr1[Integer.parseInt(a)]++; 
         }
 
-        for(String b : Y.split("")){
+        for(String b : Y.split("")){//Y에 포함된 숫자 갯수 count
             arr2[Integer.parseInt(b)]++; 
         }
 
-        for(int i = 9; i >= 0; i--){
-            if(arr1[i] > 0 && arr2[i] > 0){
+        for(int i = 9; i >= 0; i--){//큰 수 부터 차례로 추가
+            if(arr1[i] > 0 && arr2[i] > 0){//X, Y 모두 포함되어 있을 때
                 int tmp = Math.min(arr1[i], arr2[i]);
                 for(int j = 0; j < tmp; j++){
                     buf.append(i);
@@ -22,7 +22,7 @@ class Solution {
             }
         }
 
-        if(buf.length()>0){
+        if(buf.length() > 0){
             answer = buf.charAt(0) == '0' ? "0" : buf.toString();
         }else{
             answer = "-1";
