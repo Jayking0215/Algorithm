ac0d892fe98d2bff04094b8c8626a65a583a9d9a
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-08-28 10:41:41 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -7,13 +7,13 @@ class Solution {
     for(int i=0; i<clothes.length; i++) {
         map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
     }
-    
+    //2가지 이상의 종류인경우
     if(map.size() > 1) {
         int count = 1;
         for(String key : map.keySet()){
             count *= map.get(key) + 1;
         }
-        answer = count - 1;
+        answer = count - 1;//아무것도 입지 않은 경우
     }
 
         return answer;
