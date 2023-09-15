a2dc125d6f73e19fafa9e62f7e67974696157c44
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-15 10:54:40 +0900

    [Bronze V] Title: 고양이, Time: 108 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+//고양이 출력
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
