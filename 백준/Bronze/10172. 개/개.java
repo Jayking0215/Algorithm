cf2db7b1e0c9f21cd10bb248a53a6e9919959743
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-15 10:58:38 +0900

    [Bronze V] Title: 개, Time: 124 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+//강아지 출력
+//|\_/|
+//|q p|   /}
+//( 0 )"""\
+//|"^"`    |
+//||_/=\\__|
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
