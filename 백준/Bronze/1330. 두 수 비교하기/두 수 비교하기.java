8d0371e48ff915d2e1949884f0ac1dd13cfd440c
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-16 12:15:32 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,25 @@
+//첫째 줄에 다음 세 가지 중 하나를 출력한다.
+
+//A가 B보다 큰 경우에는 '>'를 출력한다.
+//A가 B보다 작은 경우에는 '<'를 출력한다.
+//A와 B가 같은 경우에는 '=='를 출력한다.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+        sc.close();
+    }
+}
