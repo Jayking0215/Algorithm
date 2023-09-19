1a60a287d1e72c0ed18fc4e4be0e8547b759a0dc
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-19 22:01:37 +0900

    [level 1] Title: 모의고사, Time: 0.55 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,21 +1,22 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int cnt1 = 0;
         int cnt2 = 0;
         int cnt3 = 0;
         
         for(int i = 0; i < answers.length; i++){
-            if(answers[i] == arr1[i % arr1.length]){
+            if(answers[i] == a[i % a.length]){
                 cnt1++;
             }
-            if(answers[i] == arr2[i % arr2.length]){
+            if(answers[i] == b[i % b.length]){
                 cnt2++;
             }
-            if(answers[i] == arr3[i % arr3.length]){
+            if(answers[i] == c[i % c.length]){
                 cnt3++;
             }
         }
