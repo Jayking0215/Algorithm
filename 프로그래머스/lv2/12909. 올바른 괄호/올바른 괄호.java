776e5c53276909bb7eb9e1bc032e733e0601c1e4
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-19 16:19:57 +0900

    [level 2] Title: 올바른 괄호, Time: 19.89 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,21 +4,21 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
         Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
+        
+        for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-
-            if (ch == '(') {
+            
+            if(ch == '('){
                 stack.push(ch);
-            } else if (ch == ')') {
-                if (stack.isEmpty() || stack.pop() != '(') {
+            }else if(ch == ')'){
+                if(stack.isEmpty() || stack.pop() != '('){
                     answer = false;
                     break;
                 }
             }
         }
-
-        if (!stack.isEmpty()) {
+        
+        if(!stack.isEmpty()){
             answer = false;
         }
 
