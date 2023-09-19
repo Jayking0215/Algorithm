0da33db41d71bb04552c2bdd36ab61462ec35799
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-19 20:03:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.31 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,15 +1,38 @@
-import java.util.*;
+import java.util.HashMap;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
         
-        for(int i = 0; i < completion.length; i++){
-            if(!participant[i].equals(completion[i])){
-                return participant[i];
+        for (String player : participant){
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        } 
+        for (String player : completion){
+            map.put(player, map.get(player) - 1);
+        } 
+
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0){
+                answer = key;
             }
         }
-        
-        return participant[participant.length - 1];
+        return answer;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public String solution(String[] participant, String[] completion) {
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
+        
+//         for(int i = 0; i < completion.length; i++){
+//             if(!participant[i].equals(completion[i])){
+//                 return participant[i];
+//             }
+//         }
+        
+//         return participant[participant.length - 1];
+//     }
+// }
