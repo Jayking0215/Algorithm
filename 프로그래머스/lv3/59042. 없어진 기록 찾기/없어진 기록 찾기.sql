04f87371e71b0bad54706ae92564504a1c221d46
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-20 13:50:55 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,6 @@
 select outs.ANIMAL_ID, outs.NAME
 from ANIMAL_OUTS as outs
 left join ANIMAL_INS as ins
-on outs.ANIMAL_ID = ins.ANIMAL_ID
+on ins.ANIMAL_ID = outs.ANIMAL_ID
 where ins.ANIMAL_ID is null
 order by outs.ANIMAL_ID;
