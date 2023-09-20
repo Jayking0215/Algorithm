02791e3a2ae4534d9587df89451ace88c1e6a097
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-20 22:55:26 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,14 @@
-select outs.ANIMAL_ID, outs.NAME
-from ANIMAL_OUTS as outs
-join ANIMAL_INS as ins
-on outs.ANIMAL_ID = ins.ANIMAL_ID
-where outs.DATETIME < ins.DATETIME
-order by ins.DATETIME;
+select o.ANIMAL_ID, o.NAME
+from ANIMAL_OUTS as o
+join ANIMAL_INS as i
+on o.ANIMAL_ID = i.ANIMAL_ID
+where o.DATETIME < i.DATETIME
+order by i.DATETIME;
+
+
+# select outs.ANIMAL_ID, outs.NAME
+# from ANIMAL_OUTS as outs
+# join ANIMAL_INS as ins
+# on outs.ANIMAL_ID = ins.ANIMAL_ID
+# where outs.DATETIME < ins.DATETIME
+# order by ins.DATETIME;
