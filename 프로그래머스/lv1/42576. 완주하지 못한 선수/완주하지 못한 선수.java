ec7f9cd470964d961fd6aee3e194cbccd08e9217
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-20 15:12:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 239.80 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,38 +1,39 @@
-import java.util.HashMap;
-
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         
-        for (String player : participant){
-            map.put(player, map.getOrDefault(player, 0) + 1);
-        } 
-        for (String player : completion){
-            map.put(player, map.get(player) - 1);
-        } 
-
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0){
-                answer = key;
+        for(int i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
             }
         }
-        return answer;
+        
+        return participant[participant.length - 1];
     }
 }
 
-// import java.util.*;
+// import java.util.HashMap;
+
 // class Solution {
 //     public String solution(String[] participant, String[] completion) {
-//         Arrays.sort(participant);
-//         Arrays.sort(completion);
+//         String answer = "";
+//         HashMap<String, Integer> map = new HashMap<>();
         
-//         for(int i = 0; i < completion.length; i++){
-//             if(!participant[i].equals(completion[i])){
-//                 return participant[i];
+//         for (String player : participant){
+//             map.put(player, map.getOrDefault(player, 0) + 1);
+//         } 
+//         for (String player : completion){
+//             map.put(player, map.get(player) - 1);
+//         } 
+
+//         for (String key : map.keySet()) {
+//             if (map.get(key) != 0){
+//                 answer = key;
 //             }
 //         }
-        
-//         return participant[participant.length - 1];
+//         return answer;
 //     }
 // }
+
