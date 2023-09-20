601a1fae66324285cf20bbb3e30245ac0c0407d4
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-20 22:17:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.88 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,42 +1,40 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        List<Integer> list = new ArrayList<>();
-        
-        list.add(arr[0]);
+    public Stack<Integer> solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
         
         for(int i = 1; i < arr.length; i++){
-            if(arr[i] != arr[i - 1]){
-                list.add(arr[i]);
+            if(stack.peek() != arr[i]){
+                stack.push(arr[i]);
             }
         }
         
-        int[] answer = new int[list.size()];
-        
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
-        }
-
-        return answer;
+        return stack;
     }
 }
 
 // import java.util.*;
 
 // public class Solution {
-//     public Stack<Integer> solution(int []arr) {
-//         Stack<Integer>stack = new Stack();
+//     public int[] solution(int []arr) {
+//         List<Integer> list = new ArrayList<>();
         
-//         stack.push(arr[0]);
+//         list.add(arr[0]);
+        
 //         for(int i = 1; i < arr.length; i++){
-//             if(stack.peek() == arr[i]){
-//                 continue;
-//             }else{
-//                 stack.push(arr[i]);
+//             if(arr[i] != arr[i - 1]){
+//                 list.add(arr[i]);
 //             }
 //         }
+        
+//         int[] answer = new int[list.size()];
+        
+//         for(int i = 0; i < list.size(); i++){
+//             answer[i] = list.get(i);
+//         }
 
-//         return stack;
+//         return answer;
 //     }
 // }
