4d35b1306de1fa0718f816b83e46cbbc9e8e2275
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-26 17:48:23 +0900

    Time: 5 ms (91.56%), Space: 43.2 MB (62.16%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,36 +1,39 @@
-// import java.util.*;
-// class Solution {
-//     public String reverseWords(String s) {
-//         String[] arr = s.split(" ");
-//         StringBuilder buf = new StringBuilder();
-        
-//         for(int i = arr.length - 1; i >= 0; i--){
-//             buf.append(arr[i]);
-//             if(i > 0){
-//                 buf.append(" ");
-//             }    
-//         }
-        
-//         return buf.toString();
-//     }
-// }
-
-
-public class Solution {
+import java.util.*;
+class Solution {
     public String reverseWords(String s) {
-        String [] arr = s.split(" ");
+        String[] arr = s.trim().split(" ");
         StringBuilder buf = new StringBuilder();
         
-        for(int i = 0; i<= arr.length - 1; i++){
-            if(!arr[i].isEmpty()) {
-                buf.insert(0, arr[i]);
-                if(i < arr.length - 1){
-                    buf.insert(0, " ");
-                }
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(!arr[i].isEmpty()){
+                buf.append(arr[i]);
+                if(i > 0){
+                    buf.append(" ");
+                }   
             }
-        
+             
         }
         
         return buf.toString();
     }
 }
+
+
+// public class Solution {
+//     public String reverseWords(String s) {
+//         String [] arr = s.split(" ");
+//         StringBuilder buf = new StringBuilder();
+        
+//         for(int i = 0; i<= arr.length - 1; i++){
+//             if(!arr[i].isEmpty()) {
+//                 buf.insert(0, arr[i]);
+//                 if(i < arr.length - 1){
+//                     buf.insert(0, " ");
+//                 }
+//             }
+        
+//         }
+        
+//         return buf.toString();
+//     }
+// }
