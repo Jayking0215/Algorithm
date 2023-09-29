99f98b131f6300a7ef57a2d35b4ffbe449a76185
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-29 12:21:22 +0900

    [level 2] Title: 숫자의 표현, Time: 0.10 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,30 +1,45 @@
 class Solution {
-    public int solution(int n){
+    public int solution(int n) {
         int answer = 0;
-
-        for(int i = 1; i <= n; i++){
-            int sum = 0;
-            for(int j = i; j <= n; j++){
-                sum += j;
-                if(sum == n){
-                    answer++;
-                    break;
-                }else if(sum > n){
-                    break;
-                }
+        
+        for(int i = 1; i <= n; i += 2){
+            if(n % i == 0){
+                answer++;
             }
         }
-
+        
         return answer;
     }
 }
 
-//자연수를 연속된 자연수의 합으로 표현하는 방법의 홀수 약수의 개수와 같다
-//public int expressions(int num) {
-//   int answer = 0;
-//   for (int i = 1; i <= num; i += 2) 
-//       if (num % i == 0) 
-//           answer++;
-//
-//   return answer;
-//}
+
+// class Solution {
+//     public int solution(int n){
+//         int answer = 0;
+
+//         for(int i = 1; i <= n; i++){
+//             int sum = 0;
+//             for(int j = i; j <= n; j++){
+//                 sum += j;
+//                 if(sum == n){
+//                     answer++;
+//                     break;
+//                 }else if(sum > n){
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
+
+// //자연수를 연속된 자연수의 합으로 표현하는 방법의 홀수 약수의 개수와 같다
+// //public int expressions(int num) {
+// //   int answer = 0;
+// //   for (int i = 1; i <= num; i += 2) 
+// //       if (num % i == 0) 
+// //           answer++;
+// //
+// //   return answer;
+// //}
