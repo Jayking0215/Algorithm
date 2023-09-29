25d1ca77be4f67f428f761825e4329f79015d41b
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-29 17:06:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,18 +1,25 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         String str = Integer.toBinaryString(n);
-        int cnt =0;
+        int answer = 0;
+        int cnt = 0;
+        
         for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == '1') cnt++;
-        }
-        for(int i = n + 1; i < 1000000; i++){
-            String bi = Integer.toBinaryString(i);
-            int bi_cnt = 0;
-            for(int j = 0; j < bi.length(); j++){
-                if(bi.charAt(j) == '1') bi_cnt++;
+            if(str.charAt(i) == '1'){
+                cnt++;
             }
-            if(bi_cnt == cnt) {
+        }
+        
+        for(int i = n + 1; i < 1000000; i++){
+            String bi_str = Integer.toBinaryString(i);
+            int bi_cnt = 0;
+            
+            for(int j = 0; j < bi_str.length(); j++){
+                if(bi_str.charAt(j) == '1'){
+                    bi_cnt++;
+                }
+            }
+            if(cnt == bi_cnt){
                 answer = i;
                 break;
             }
@@ -22,8 +29,34 @@ class Solution {
     }
 }
 
-//2진수의 1의 갯수 cnt를 
-//String bi = Integer.toBinaryString(i);
-//int bi_cnt = bi.replaceAll("0", "").length();
-//에서
-//for문으로 변경(효율성 부분에서 for문이 더 유리함)
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         String str = Integer.toBinaryString(n);
+//         int cnt =0;
+        
+//         for(int i = 0; i < str.length(); i++){
+//             if(str.charAt(i) == '1') cnt++;
+//         }
+        
+//         for(int i = n + 1; i < 1000000; i++){
+//             String bi = Integer.toBinaryString(i);
+//             int bi_cnt = 0;
+//             for(int j = 0; j < bi.length(); j++){
+//                 if(bi.charAt(j) == '1') bi_cnt++;
+//             }
+//             if(bi_cnt == cnt) {
+//                 answer = i;
+//                 break;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+// //2진수의 1의 갯수 cnt를 
+// //String bi = Integer.toBinaryString(i);
+// //int bi_cnt = bi.replaceAll("0", "").length();
+// //에서
+// //for문으로 변경(효율성 부분에서 for문이 더 유리함)
