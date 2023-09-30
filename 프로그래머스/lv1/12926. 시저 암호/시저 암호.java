43981d5f37bcd3ab88767b3a5b24b47bcaae4d32
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-30 17:03:21 +0900

    [level 1] Title: 시저 암호, Time: 2.99 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,29 +1,60 @@
-public class Solution {
-	public String solution(String s, int n) {
-		String answer = "";
+class Solution {
+    public String solution(String s, int n) {
         StringBuilder buf = new StringBuilder();
         
-		for(int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-            if(ch == ' ') {
-				buf.append(ch);
-				continue;
-			}
-			if(ch >= 'a' && ch <= 'z') {//소문자인 경우
-				if(ch + n > 'z') {//'z'를 넘어갈 경우
-					buf.append((char)(ch - 26 + n));
-				}else {
-					buf.append((char)(ch + n));
-				}
-			}else if(ch >= 'A' && ch <= 'Z') {//대문자인 경우
-				if(ch + n > 'Z') {//'Z'를 넘어갈 경우
-					buf.append((char)(ch - 26 + n));
-				}else {
-					buf.append((char)(ch + n));
-				}
-			}
-		}
-        answer = buf.toString();
-		return answer;
-	}
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(ch == ' '){
+                buf.append(' ');
+                continue;
+            }
+            if('a' <= ch && 'z' >= ch){
+                if(ch + n > 'z'){
+                    buf.append((char)(ch - 26 + n));
+                }else{
+                    buf.append((char)(ch + n));
+                }
+            }else if('A' <= ch && 'Z' >= ch){
+                if(ch + n > 'Z'){
+                    buf.append((char)(ch - 26 + n));
+                }else{
+                    buf.append((char)(ch + n));
+                }
+            }
+        }
+        
+        return buf.toString();
+    }
 }
+
+// public class Solution {
+// 	public String solution(String s, int n) {
+// 		String answer = "";
+//         StringBuilder buf = new StringBuilder();
+        
+// 		for(int i = 0; i < s.length(); i++) {
+// 			char ch = s.charAt(i);
+            
+//             if(ch == ' ') {//공백처리
+// 				buf.append(ch);
+// 				continue;
+// 			}
+// 			if(ch >= 'a' && ch <= 'z') {//소문자인 경우
+// 				if(ch + n > 'z') {//'z'를 넘어갈 경우
+// 					buf.append((char)(ch - 26 + n));
+// 				}else {
+// 					buf.append((char)(ch + n));
+// 				}
+// 			}else if(ch >= 'A' && ch <= 'Z') {//대문자인 경우
+// 				if(ch + n > 'Z') {//'Z'를 넘어갈 경우
+// 					buf.append((char)(ch - 26 + n));
+// 				}else {
+// 					buf.append((char)(ch + n));
+// 				}
+// 			}
+// 		}
+//         answer = buf.toString();
+// 		return answer;
+// 	}
+// }
