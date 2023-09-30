c399db0f3baae57875ddae4fa86a86445b648caa
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-09-30 17:41:59 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        s = s.replace("zero", "0")
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
+
+// class Solution {
+//     public int solution(String s) {
+//         int answer = 0;
+//         //String str=s;
+//         s=s.replace("zero", "0")
+//              .replace("one", "1")
+//              .replace("two", "2")
+//              .replace("three", "3")
+//              .replace("four", "4")
+//              .replace("five", "5")
+//              .replace("six", "6")
+//              .replace("seven", "7")
+//              .replace("eight", "8")
+//              .replace("nine", "9");
+//         answer=Integer.parseInt(s);
+//         return answer;
+//     }
+// }
+
+// // String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+// // for(int i = 0; i < strArr.length; i++) {
+// //     s = s.replaceAll(strArr[i], Integer.toString(i));
+// // }
