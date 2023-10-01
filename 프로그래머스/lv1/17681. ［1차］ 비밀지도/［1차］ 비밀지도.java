c63b3d656b8561b667900e42c25432eff66ad774
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-10-01 17:54:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.47 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,13 +1,19 @@
+//비트연산 | (OR연산...1이면 1로 변환) &(둘 다 1일경우 1로 변환) ^(다를경우 1)
+//이진수로 변환  toBinaryString
+//앞자리 0 채우기
+//.replaceAll("1", "#")
+//.replaceAll("0", " ")
+
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
-        for (int i = 0; i < n; i++) {
-            int birOr = arr1[i] | arr2[i];
-            String str = Integer.toBinaryString(birOr);
+        for(int i = 0; i < n; i++){
+            int bitOr = arr1[i] | arr2[i];
+            String str = Integer.toBinaryString(bitOr);
             
-            while (str.length() < n) {
-                str = "0" + str; // 부족한 자릿수 채우기
+            while(str.length() < n){
+                str = "0" + str;
             }
             
             str = str.replaceAll("1", "#");
@@ -15,7 +21,29 @@ class Solution {
             
             answer[i] = str;
         }
-        
         return answer;
     }
 }
+
+
+// class Solution {
+//     public String[] solution(int n, int[] arr1, int[] arr2) {
+//         String[] answer = new String[n];
+        
+//         for (int i = 0; i < n; i++) {
+//             int birOr = arr1[i] | arr2[i];
+//             String str = Integer.toBinaryString(birOr);
+            
+//             while (str.length() < n) {
+//                 str = "0" + str; // 부족한 자릿수 채우기
+//             }
+            
+//             str = str.replaceAll("1", "#");
+//             str = str.replaceAll("0", " ");
+            
+//             answer[i] = str;
+//         }
+        
+//         return answer;
+//     }
+// }
