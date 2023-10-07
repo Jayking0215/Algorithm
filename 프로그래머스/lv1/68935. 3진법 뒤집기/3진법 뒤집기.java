5573f7d0a2546e6436f079eaaae3fea967a52b46
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-10-07 20:07:08 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,12 +1,28 @@
-import java.util.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         StringBuilder buf = new StringBuilder();
-        while(n != 0) {
+        int answer = 0;
+        
+        while(n != 0){//3진법 역순
             buf.append(n % 3);
             n /= 3;
         }
-        return Integer.parseInt(buf.toString(), 3);
+        
+        answer = Integer.parseInt(buf.toString(), 3);
+        
+        return answer;
     }
 }
+
+// import java.uti l.*;
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         StringBuilder buf = new StringBuilder();
+//         while(n != 0) {
+//             buf.append(n % 3);
+//             n /= 3;
+//         }
+//         return Integer.parseInt(buf.toString(), 3);
+//     }
+// }
