6d8361e4af023fd9f90343e53d88a1d12b37aab7
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-10-12 21:18:06 +0900

    Time: 0 ms (100.00%), Space: 39.9 MB (6.00%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,36 +1,40 @@
-// class Solution {
-//     public int numberOfSteps(int num) {
-//         int cnt = 0;
+class Solution {
+    public int numberOfSteps(int num) {
+        int cnt = 0;
         
-//         while(num != 0){
-//             if(num % 2 == 0){
-//                 num /= 2;
-//                 cnt++;
-//             }else{
-//                 num -= 1;
-//                 cnt++;
-//             }
-//         }
+        while(num != 0){
+            if(num % 2 == 0){
+                num /= 2;
+                cnt++;
+            }else{
+                num -= 1;
+                cnt++;
+            }
+        }
         
-//         return cnt;
-//     }
-// }
+        return cnt;
+    }
+}
+
 //이해 안됨...
 // public int numberOfSteps (int num) {
 // 	return Integer.toBinaryString(num).length() - 1 + Integer.bitCount(num);
 // }
-class Solution{
-    public int numberOfSteps(int num) {
-        int count = 0;
 
-        while (num > 0) {
-            num = (num & 1) == 1 ? num ^ 1 : num >> 1;
-            count++;
-        }
-        return count;
-    }
-}
+// class Solution{
+//     public int numberOfSteps(int num) {
+//         int count = 0;
+
+//         while (num > 0) {
+//             num = (num & 1) == 1 ? num ^ 1 : num >> 1;
+//             count++;
+//         }
+//         return count;
+//     }
+// }
 //(num & 1) ... 짝수 판별 비트연산
 //결과가 1이면 홀수
 //num ^ 1...가장 오른쪽 비트 반전 = 1 빼는것과 같다
 //num >> 1...오른쪽 1 시프트 연산 = 2로 나눈것과 같다 ex) 1000(=8) >> 1 == 0100(=4)
+
+//1,3풀이에 대한 차이는 없음
