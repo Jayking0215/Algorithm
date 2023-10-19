76f7c33c4ec26ddd67200a08d2061587ad16b03c
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-10-19 16:39:16 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int total = brown + yellow;
+        int sqrt = (int)Math.sqrt(total);
+        
+        for(int i = sqrt; i > 0; i--){
+            if(total % i == 0){
+                int w = total / i;
+                int h = i;
+                
+                int yt = (w - 2) * (h - 2);
+                if(yt == yellow){
+                    answer[0] = w;
+                    answer[1] = h;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
