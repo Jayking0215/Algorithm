dc9c2b2023bda4fe811a443f95052b66ab7e6020
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-10-20 15:14:18 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,17 @@
 class Solution {
-
-    static int answer = 0;
-    
-    public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
-        
-        return answer;
+    public int solution(int[] numbers, int target){
+        return dfs(numbers, target, 0, 0);
     }
     
-    public void dfs(int[] numbers, int target, int idx, int sum){
-    	// 배열 마지막 노드까지 탐색이 끝났는지 체크
+    public int dfs(int[] numbers, int target, int idx, int sum){
         if(idx == numbers.length){
-        	// 합계와 타겟 값이 같으면 answer을 1 증가 시킴.
-            if(sum == target) answer++; 
-        }else{
-        	// 해당 노드 값을 더하고 다음 값 탐색
-            dfs(numbers, target, idx + 1, sum + numbers[idx]);
-            // 해당 노드 값을 빼고 다음 값 탐색
-            dfs(numbers, target, idx + 1, sum - numbers[idx]);
-        }   
+            return sum == target ? 1 : 0;
+        }
+        
+        int answer = 0;
+        answer += dfs(numbers, target, idx + 1, sum + numbers[idx]);
+        answer += dfs(numbers, target, idx + 1, sum - numbers[idx]);
+        
+        return answer;
     }
 }
