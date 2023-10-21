037a83a0aa1c5dc308178f9d639c76d8eb243e82
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-10-21 20:03:47 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String input = bf.readLine().trim();
+            boolean isBalanced = isBalancedParentheses(input);
+            System.out.println(isBalanced ? "YES" : "NO");
+        }
+    }
+
+    public static boolean isBalancedParentheses(String s) {
+        int balance = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                balance++;
+            } else if (c == ')') {
+                balance--;
+            }
+
+            if (balance < 0) {
+                return false;
+            }
+        }
+
+        return balance == 0;
+    }
+}
