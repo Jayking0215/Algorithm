c91ed1889899739d9086b0f5f0664d8772af7f2e
JJJ <61086704+Jayking0215@users.noreply.github.com>
2023-12-10 09:44:26 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        switch(n / 10) {
+            case 10:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+        }
+    }
+}
